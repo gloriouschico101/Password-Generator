812ca3dfb0d974fb8c978f77a8647bfaf9df1728
gloriouschico101 <20029630@tafe.wa.edu.au>
2022-08-12 15:54:49 +0800

    Allowed user to input length of password

--- Password-Generator.py
+++ Password-Generator.py
@@ -5,7 +5,13 @@ letters = string.ascii_letters
 
 password = ""
 
-for i in range(10):
-    password = password + random.choice(letters)
+length = int(input("Enter a password length between 0-50: "))
+
+if length < 0 or length > 50:
+    print("Error: Please enter a number between 0 and 50")
+
+else:
+    for i in range(length):
+        password = password + random.choice(letters)
 
 print(password)
